dd49236428813a3ddfd72913033392c20af30efa
anthony-bt <aa.barillot@gmail.com>
2019-03-23 16:26:33 +0100

    Add navigation menu

--- MyFirstApp/App.js
+++ MyFirstApp/App.js
@@ -6,44 +6,58 @@
  * @flow
  */
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import React from "react";
+import { Image, TouchableOpacity, StyleSheet } from "react-native";
+import { createDrawerNavigator, createStackNavigator, createAppContainer } from "react-navigation";
+import { LoginScreen } from "./src/screens/LoginScreen";
+import { HomeScreen } from "./src/screens/HomeScreen";
+import { DetailsScreen } from "./src/screens/DetailScreen";
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
-type Props = {};
-export default class App extends Component<Props> {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
-    );
-  }
-}
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  icon: {
+    width: 46,
+    height: 46,
   },
 });
+
+const DrawerStack = createDrawerNavigator({
+  Home: { screen: HomeScreen },
+  Details: { screen: DetailsScreen },
+}, {
+  initialRouteName: 'Home',
+  headerMode: 'float',
+  navigationOptions: ({navigation}) => ({
+    headerStyle: {backgroundColor: '#f4511e'},
+    title: 'MyFirstApp',
+    headerTintColor: 'white',
+    headerLeft: <TouchableOpacity onPress={() => navigation.toggleDrawer()}>
+                  <Image source={require('./src/assets/header-menu.png')}
+                         style={[styles.icon]} />
+                </TouchableOpacity>,
+  }),
+  contentOptions: {
+    activeTintColor: '#e91e63',
+  },
+})
+
+const DrawerNavigation = createStackNavigator({
+  DrawerStack: { screen: DrawerStack }
+})
+
+const AppNavigator = createStackNavigator({
+  Login: { screen: LoginScreen },
+  drawerStack: { screen: DrawerNavigation }
+}, {
+  headerMode: 'none',
+  title: 'Main',
+  initialRouteName: 'Login'
+})
+
+const AppContainer = createAppContainer(AppNavigator);
+
+export default class App extends React.Component {
+  render() {
+    return <AppContainer />;
+  }
+}
